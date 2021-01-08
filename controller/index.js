1e886e348c4bc092ef7194bb90c51d3ac5ca31df
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-01-08 00:01:34 -0800

    Work on mvc setup

--- controller/index.js
+++ controller/index.js
@@ -3,7 +3,9 @@ const model = require(../model/index.js);
 module.exports = {
   get: {
     images: (shoeID) => {
-
+      return new Promise((resolve, reject) => {
+        model.getShoeImages(shoeID)
+        .then((images))
     }
   }
 }
