ddf09d29c82149643ea1bbb17180a562d47d731a
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-01-09 12:15:19 -0800

    Begin react hooks component

--- client/components/Gallery.jsx
+++ client/components/Gallery.jsx
@@ -1,7 +1,19 @@
 import React, { useState, useEffect }from 'react';
 import ReactDOM from 'react-dom';
+const axios = require('axios');
 
 const Gallery = (props) => {
+
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    let shoeId = props.modelId
+    axios.get(`/products/${shoeId}/gallery`)
+      .then((images) => {
+        console.log(images);
+        setImages(images);
+      });
+  }, [])
 
   return (
     <div>
--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from './components/Gallery.jsx';
+let params = new URLSearchParams(window.location.search);
+let val = params.get('prod');
+let productId = val ? val : 1;
 
-ReactDOM.render(<Gallery />, document.getElementById('image-container'));
+ReactDOM.render(<Gallery modelId={productId}/>, document.getElementById('image-container'));
