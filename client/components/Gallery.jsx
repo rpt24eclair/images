78c1b9d7b5732fb13ad82d76cb3f171f1a0b1074
Gabriel Gutierrez <gabe.grez@gmail.com>
2021-01-09 23:48:02 -0800

    Add modal functionality

--- client/components/Gallery.jsx
+++ client/components/Gallery.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect }from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import ImageBox from './ImageBox.jsx';
 import styles from './css/Gallery.module.css';
 const axios = require('axios');
@@ -24,34 +25,90 @@ const Gallery = (props) => {
 
   const buildImageComponents = (imageUrls) => {
     return imageUrls.map((image, i)=> {
-      return <ImageBox url={image} location={i} />
+      return <ImageBox url={image} location={i} click={openModal}/>
     });
   }
+  /////////// modal settings and functions ///////////
+  const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
 
+  var subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
 
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+  /////////// end of modal s/f ///////////
   return (
     <div className={styles.wrapper}>
-      {/* {render && buildImageComponents(images)} */}
-      <div className={styles.imageCell}>
-        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
-      </div>
-      <div className={styles.imageCell}>
-        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
-      </div>
-      <div className={styles.imageCell}>
-        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
-      </div>
-      <div className={styles.imageCell}>
-        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
-      </div>
-      <div className={styles.imageCell}>
-        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
-      </div>
-      <div className={styles.imageCell}>
-        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
-      </div>
+      {render && buildImageComponents(images)}
+      <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
+          <button onClick={closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
     </div>
   )
 }
 
 export default Gallery
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className={styles.imageCell}>
+        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
+      </div>
+      <div className={styles.imageCell}>
+        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
+      </div>
+      <div className={styles.imageCell}>
+        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
+      </div>
+      <div className={styles.imageCell}>
+        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
+      </div>
+      <div className={styles.imageCell}>
+        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
+      </div>
+      <div className={styles.imageCell}>
+        <img className={styles.image} src="https://source.unsplash.com/random/1000x1000" alt="Example image"/>
+      </div> */}
--- client/components/ImageBox.jsx
+++ client/components/ImageBox.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import styles from './css/Gallery.module.css';
 
 const ImageBox = (props) => {
 
+  // const [modalStatus, setModalStatus] = useState(false);
+  // const [index, setIndex] = useState(props.i);
+
+  const handleClick = (e) => {
+    props.click();
+  }
+
+  /////// MODAL SETTINGS /////////
+  // const customStyles = {
+  //   content : {
+  //     top                   : '50%',
+  //     left                  : '50%',
+  //     right                 : 'auto',
+  //     bottom                : 'auto',
+  //     marginRight           : '-50%',
+  //     transform             : 'translate(-50%, -50%)'
+  //   }
+  // };
+
+  // var subtitle;
+  // const [modalIsOpen,setIsOpen] = useState(false);
+  // const openModal = () => {
+  //   setIsOpen(true);
+  // }
+
+  // const afterOpenModal = () => {
+  //   // references are now sync'd and can be accessed.
+  //   subtitle.style.color = '#f00';
+  // }
+
+  // const closeModal = () => {
+  //   setIsOpen(false);
+  // }
+
   return (
-    <div className={styles.imageCell}>
+    <div className={styles.imageCell} onClick={() => handleClick()}>
       <img className={styles.image} key={props.i} src={props.url} height={1000} width={1000}/>
     </div>
   )
