7b0846b5f977ae77f1a3efe286185d6ca7ee6346
Jake <jvanboch@gmail.com>
2021-02-03 19:39:29 -0800

    updated seed script to generate 2-6 shoes per model, updated table to be just the image number, updated get route to provide image based on image number

--- database/seed.sql
+++ database/seed.sql
@@ -1,6 +1,6 @@
 USE sbGallery;
 set global local_infile = 1;
-LOAD DATA LOCAL INFILE '/home/jvanboch/my_git/repos/SDC/somebirds-product-images/database/model1.csv' INTO
+LOAD DATA LOCAL INFILE '/home/jvanboch/my_git/repos/SDC/somebirds-product-images/database/test0.csv' INTO
 TABLE Images FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
